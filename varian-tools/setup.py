a7963e68e1baf8b4a742f3f1b90357fa46eea7c8
Brian Hawthorne <brian.lee.hawthorne@gmail.com>
2005-08-31 07:57:52 +0000

    fix to work with new structure of varian-tools project
    git-svn-id: file:///home/mb312/repositories/public-repo/bic/trunk@593 c5881a9f-4ef2-0310-b211-a1cdbd18b07b

--- varian-tools/setup.py
+++ varian-tools/setup.py
@@ -2,12 +2,12 @@
 from distutils.core import setup
 
 setup(
-  name='python-varian',
+  name='python-varian-tools',
   version='0.1',
   description='Tools for reading Varian scan output.',
   author='Brian Hawthorne',
   author_email='brian.lee.hawthorne@gmail.com',
   url='http://cirl.berkeley.edu/view/BIC/VarianTools',
   packages=['varian', 'varian.lib', 'varian.tools'],
-  scripts=['varian/bin/varian.dumpheader', 'varian/bin/varian.getparam']
+  scripts=['varian-dumpheader', 'varian-getparam']
 )
