0f38fcd1a5664fe8a911e9649cf84dda476a30af
Brian Hawthorne <brian.lee.hawthorne@gmail.com>
2005-10-26 06:10:16 +0000

    - moved cfg file to top of the project - modified test.sh to create the output dir if necessary
    git-svn-id: file:///home/mb312/repositories/public-repo/bic/trunk@614 c5881a9f-4ef2-0310-b211-a1cdbd18b07b

--- epi-recon/test.sh
+++ epi-recon/test.sh
@@ -2,10 +2,12 @@
 export PYTHONPATH=./root
 recon_cmd=./recon
 testdata_dir=../testdata
+output_dir=./Images
 #dataset=Vari_ss_epi
 dataset=Ravi_ns22
 
+mkdir -p $output_dir
 python $recon_cmd --config=recon.cfg --file-format=spm --phs-corr=nonlinear \
     $testdata_dir/$dataset.fid/fid \
     $testdata_dir/$dataset.fid/procpar \
-    ./Images/${dataset}_recon 
+    ./$output_dir/${dataset}_recon 
