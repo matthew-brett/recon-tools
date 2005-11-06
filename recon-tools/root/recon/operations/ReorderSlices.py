86dd7a91072c87b13bf18073cb47460143027fa8
Brian Hawthorne <brian.lee.hawthorne@gmail.com>
2005-11-06 03:00:38 +0000

    simplified and optimized ReorderSlices operation
    git-svn-id: file:///home/mb312/repositories/public-repo/bic/trunk@634 c5881a9f-4ef2-0310-b211-a1cdbd18b07b

--- recon-tools/root/recon/operations/ReorderSlices.py
+++ recon-tools/root/recon/operations/ReorderSlices.py
@@ -1,5 +1,5 @@
 from Numeric import empty
-from pylab import mlab
+from pylab import mlab, zeros, arange, take
 from recon.operations import Operation, Parameter
 
 ##############################################################################
@@ -14,23 +14,10 @@ class ReorderSlices (Operation):
     def run(self, options, data):
         nslice = data.nslice
         imgdata = data.data_matrix
-
-        # Reorder the slices from inferior to superior.
+        # this needs testing on odd number of slices
         midpoint = nslice/2 + (nslice%2 and 1 or 0)
-        tmp = empty(imgdata[0].shape, imgdata.typecode())
-        #print "midpoint=",midpoint
-        for volume in imgdata:
-            # if I can get the slice indices right, these two lines can replace
-            # the nine lines which follow them! - BH
-            #tmp[:midpoint] = self.flip_slices and volume[::2] or volume[::-2]
-            #tmp[midpoint:] = self.flip_slices and volume[1::2] or volume[-2::-2]
-            for i, slice in enumerate(volume):
-                if i < midpoint:
-                    if self.flip_slices: z = 2*i
-                    else: z = nslice - 2*i - 1
-                else:
-                    if self.flip_slices: z = 2*(i - midpoint) + 1
-                    else: z = nslice - 2*(i - midpoint) - 2
-                #print i, z
-                tmp[z] = slice
-            volume[:] = tmp
+        tmp = empty(imgdata.shape, imgdata.typecode())
+        indices = nslice - 1 - arange(nslice)
+        tmp[:,::2] = take(imgdata, indices[:midpoint], axis=1)
+        tmp[:,1::2] = take(imgdata, indices[midpoint:], axis=1)
+        imgdata[:] = self.flip_slices and take(tmp, indices, axis=1) or tmp
