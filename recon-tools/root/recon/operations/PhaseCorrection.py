78d6aa073516d417d30142906af9f6aa0edc570e
Brian Hawthorne <brian.lee.hawthorne@gmail.com>
2005-11-08 00:47:31 +0000

    simplify PhaseCorrection
    git-svn-id: file:///home/mb312/repositories/public-repo/bic/trunk@639 c5881a9f-4ef2-0310-b211-a1cdbd18b07b

--- recon-tools/root/recon/operations/PhaseCorrection.py
+++ recon-tools/root/recon/operations/PhaseCorrection.py
@@ -9,13 +9,13 @@ class PhaseCorrection (Operation):
 
     #-------------------------------------------------------------------------
     def run(self, options, data):
-        ksp_data = data.data_matrix
+        imgdata = data.data_matrix
 
         # phase angle of inverse fft'd reference volume
         ref_phs = angle(inverse_fft(data.ref_data))
 
-        # Apply the phase correction to the image data.
-        for volume in ksp_data:
-            for slice, theta in zip(volume, ref_phs):
-                cor = cos(-theta) + 1.0j*sin(-theta)
-                slice[:] = fft(inverse_fft(slice)*cor).astype(Complex32)
+        # compute phase correction
+        cor = cos(-ref_phs) + 1.j*sin(-ref_phs)
+
+        # apply correction to image data
+        data.data_matrix = fft(inverse_fft(imgdata)*cor).astype(Complex32)
