3f29d2eb35563bc1235e8e6e85496ca797b7586e
Brian Hawthorne <brian.lee.hawthorne@gmail.com>
2005-11-09 11:14:21 +0000

    operation for flipping data slices about the horizontal and vertical axes
    git-svn-id: file:///home/mb312/repositories/public-repo/bic/trunk@664 c5881a9f-4ef2-0310-b211-a1cdbd18b07b

---
+++ imaging-tools/root/imaging/operations/FlipData.py
@@ -0,0 +1,25 @@
+from pylab import flipud, fliplr
+from imaging.operations import Operation, Parameter
+
+##############################################################################
+class FlipData (Operation):
+    "Flip image slices up-down and left-right"
+
+    params=(
+      Parameter(name="flipud", type="bool", default=True,
+        description="flip each slice up-down"),
+      Parameter(name="fliplr", type="bool", default=True,
+        description="flip each slice left-right"))
+
+    #-------------------------------------------------------------------------
+    def run(self, options, data):
+        if not self.flipud and not self.fliplr: return
+        for volume in data.data_matrix:
+            for slice in volume:
+                if self.flipud and self.fliplr:
+                    newslice = flipud(fliplr(slice))
+                elif self.flipud:
+                    newslice = flipud(slice)
+                elif self.fliplr:
+                    newslice = fliplr(slice)
+                slice[:] = newslice
