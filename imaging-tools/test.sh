20177d3889317eab912386a3013c4d82184f938c
Brian Hawthorne <brian.lee.hawthorne@gmail.com>
2005-11-09 07:08:51 +0000

    change recon.cfg file to recon.ops
    git-svn-id: file:///home/mb312/repositories/public-repo/bic/trunk@662 c5881a9f-4ef2-0310-b211-a1cdbd18b07b

--- imaging-tools/test.sh
+++ imaging-tools/test.sh
@@ -7,4 +7,4 @@ output_dir=./Images
 dataset=Ravi_ns22
 
 mkdir -p $output_dir
-python $recon_cmd --config=recon.cfg $testdata_dir/$dataset.fid ./$output_dir/${dataset}_recon 
+python $recon_cmd --config=recon.ops $testdata_dir/$dataset.fid ./$output_dir/${dataset}_recon 
