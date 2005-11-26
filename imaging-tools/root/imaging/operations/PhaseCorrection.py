f26c5d078ab2eabc3ad05c435799d323a0bd1a0e
Brian Hawthorne <brian.lee.hawthorne@gmail.com>
2005-11-26 02:09:53 +0000

    cosmetic
    git-svn-id: file:///home/mb312/repositories/public-repo/bic/trunk@681 c5881a9f-4ef2-0310-b211-a1cdbd18b07b

--- imaging-tools/root/imaging/operations/PhaseCorrection.py
+++ imaging-tools/root/imaging/operations/PhaseCorrection.py
@@ -14,7 +14,7 @@ class PhaseCorrection (Operation):
         ref_phs = angle(inverse_fft(image.ref_data))
 
         # compute phase correction
-        cor = cos(-ref_phs) + 1.j*sin(-ref_phs)
+        correction = cos(-ref_phs) + 1.j*sin(-ref_phs)
 
         # apply correction to image data
-        image.data = fft(inverse_fft(image.data)*cor).astype(Complex32)
+        image.data = fft(inverse_fft(image.data)*correction).astype(Complex32)
