6e9f852c515069b449c51769cf0e0767d18bf509
Brian Hawthorne <brian.lee.hawthorne@gmail.com>
2006-01-18 21:53:48 +0000

    new operation for computing the field map of an asems image
    git-svn-id: file:///home/mb312/repositories/public-repo/bic/trunk@696 c5881a9f-4ef2-0310-b211-a1cdbd18b07b

---
+++ imaging-tools/root/imaging/operations/ComputeFieldMap.py
@@ -0,0 +1,123 @@
+#import file_io
+from Numeric import *
+from LinearAlgebra import *
+import math
+#from imaging.util import median_filter
+from imaging.imageio import write_analyze
+from imaging.operations import Operation, Parameter
+
+#-----------------------------------------------------------------------------
+def exec_cmd(cmd, verbose=False, exit_on_error=False):
+    "Execute unix command and handle errors."
+    import sys, os
+    if(verbose): print "\n" +  cmd
+    status = os.system(cmd)
+    if(status):
+        print "\n****** Error occurred while executing: ******"
+        print cmd
+        print "Aborting procedure\n\n"
+        if exit_on_error: sys.exit(1)
+
+
+##############################################################################
+class ComputeFieldMap (Operation):
+    "Perform phase unwrapping and calculate field map"
+
+    #-------------------------------------------------------------------------
+    def run(self, image):
+        if not hasattr(image._procpar, "asym_time"):
+            self.log("No asym_time, can't compute field map.")
+            return
+
+        # Get procpar info.
+        nvol = image.nvol
+        #asym_time = image._procpar.asym_time
+        #print "asym_time:",asym_time
+
+        # Create image filenames.
+        trange = range(nvol)
+        ksp = ["ksp_%04d.img"%t for t in trange]
+        ksp_hdr = ["ksp_%04d.hdr"%t for t in trange]
+        phs_unwrapped = ["ksp_phs_unwrapped_%04d.img"%t for t in trange]
+        phs_unwrapped_hdr = ["ksp_phs_unwrapped_%04d.hdr"%t for t in trange]
+        #fmap_files = ["fmap_%02d.img"%t for t in trange[:-1]]
+        #pixshift_files = ["pixshift_%02d.img"%t for t in trange[:-1]]
+        #fmap_file_fitted = "fmap_fitted.img"
+        #phase_pair = "phase_pair.img"
+        #phase_pair_hdr = "phase_pair.hdr"
+       
+        # Unwrap phases.
+        for idx in range(nvol):
+            exec_cmd("prelude --complex=%s --unwrap=%s -v -t 2000"%\
+              (ksp[idx], phs_unwrapped[idx]))
+ 
+        # Read header info and set for output.
+        #hdr = file_io.read_header(ksp[0])
+        #xdim = hdr['xdim']
+        #ydim = hdr['ydim']
+        #zdim = hdr['zdim']
+        #xsize = hdr['xsize']
+        #ysize = hdr['ysize']
+        #zsize = hdr['zsize']
+        #datatype = 3 # Floating point
+
+        #for idx in range(nvol-1):
+
+            # Concatenate adjacent unwrapped phase maps into a single 2-frame file.
+        #    exec_cmd("cat %s %s > %s"%(phs_unwrapped[idx],phs_unwrapped[idx+1],phase_pair))
+        #    hdr = file_io.create_hdr(xdim,ydim,zdim,2,xsize,ysize,zsize,1.,0,0,0,'Float',32,1.,'analyze',phase_pair,0)
+        #    file_io.write_analyze_header(phase_pair_hdr,hdr)
+
+            # Compute B-maps for concatenated unwrapped phase maps.
+        #    exec_cmd("fugue -p %s --asym=%f --savefmap=%s --dwell=1. --saveshift=%s"%\
+        #      (phase_pair,asym_time,fmap_files[idx],pixshift_files[idx]))
+
+        # Use a linear regression to fit phases, then use the the fit to estimate field map.
+        #if nvol > 2:
+        #    sumx = 0.
+        #    sumxsq = 0.
+        #    sumy = zeros((zdim,ydim,xdim)).astype(Float)
+        #    sumxy = zeros((zdim,ydim,xdim)).astype(Float)
+        #    phasem1 = zeros((zdim,ydim,xdim)).astype(Float)
+        #    for t in range(nvol):
+        #        phs_data = file_io.read_file(phs_unwrapped[t])
+        #        phase = phs_data['image']
+        #        if t > 0:
+                    # Ensure that phase is within 2*pi of the last phase values (Acq protocol should not allow phase wraps.)
+        #            diff = phase - phasem1
+        #            mask = where(equal(diff,0.),0.,1.)
+        #            N = sum(mask.flat)
+        #            offset = int(sum(diff.flat)/(N*2.*math.pi))
+        #            print t,N,offset
+        #            if offset != 0.:
+        #                phase = phase - 2.*math.pi*offset*mask
+        #                file_io.write_analyze("phase.img",phs_data['header'],phase)
+        #        sumx = sumx + t
+        #        sumxsq = sumxsq + t**2
+        #        sumy = sumy + phase[:,:,:]
+        #        sumxy = sumxy + t*phase[:,:,:]
+        #        phasem1[:,:,:] = phase
+        #    fmap = (nvol*sumxy - sumx*sumy)/(asym_time*(nvol*sumxsq - sumx**2))
+        #    fmap = median_filter(fmap,3)
+        #    fmap_hdr = file_io.create_hdr(xdim,ydim,zdim,1,xsize,ysize,zsize,1.,0,0,0,'Float',32,1.,'analyze',fmap_file_fitted,0)
+        #    file_io.write_analyze(fmap_file_fitted,fmap_hdr,fmap)
+
+        # cleanup leftover files
+        sp = " "
+        exec_cmd("/bin/rm %s %s %s %s %s %s %s %s"%(
+          sp.join(ksp), sp.join(ksp_hdr),
+          sp.join(phs_unwrapped), sp.join(phs_unwrapped_hdr),
+          phase_pair, phase_pair_hdr))
+
+        #======== Move this dwell time stuff into ProcParImageMixin =========
+        # Determine dwell time.
+        #if image._procpar.has_key('dwell'):
+            #dwell_time = image._procpar.dwell
+        #else:
+            #trise = image._procpar.trise
+            #gro = image._procpar.gro
+            #gmax = image._procpar.gmax
+            #np = image._procpar.np
+            #sw = image._procpar.sw
+            # This one give values that agree with Varian's
+            #dwell_time = (trise*gro/gmax) + np/(2.*sw)
