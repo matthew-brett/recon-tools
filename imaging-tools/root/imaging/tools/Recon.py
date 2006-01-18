f25edfe18eb285d8e84f81a0feb31078834353d9
Brian Hawthorne <brian.lee.hawthorne@gmail.com>
2006-01-18 21:48:09 +0000

    - moved OrderedConfigParser into the containing package so it can be shared by other tools - refactored options declarations - added runlog functionality
    git-svn-id: file:///home/mb312/repositories/public-repo/bic/trunk@689 c5881a9f-4ef2-0310-b211-a1cdbd18b07b

--- imaging-tools/root/imaging/tools/Recon.py
+++ imaging-tools/root/imaging/tools/Recon.py
@@ -1,40 +1,27 @@
-from ConfigParser import SafeConfigParser
 from optparse import OptionParser, Option
+
+from imaging.tools import OrderedConfigParser
 from imaging.varian.FidImage import FIDL_FORMAT, VOXBO_FORMAT, ANALYZE_FORMAT,\
   MAGNITUDE_TYPE, COMPLEX_TYPE, FidImage
-from imaging.operations import OperationManager
-
-output_format_choices = (FIDL_FORMAT, VOXBO_FORMAT, ANALYZE_FORMAT)
-output_datatype_choices= (MAGNITUDE_TYPE, COMPLEX_TYPE)
-
-
-##############################################################################
-class OrderedConfigParser (SafeConfigParser):
-    "Config parser which keeps track of the order in which sections appear."
-
-    #-------------------------------------------------------------------------
-    def __init__(self, defaults=None):
-        SafeConfigParser.__init__(self, defaults=defaults)
-        import odict
-        self._sections = odict.odict()
+from imaging.operations import OperationManager, RunLogger
 
 
 ##############################################################################
 class Recon (OptionParser):
     "Handle command-line aspects of the recon tool."
 
-    #-------------------------------------------------------------------------
-    def __init__(self, *args, **kwargs):
-        OptionParser.__init__(self, *args, **kwargs)
-        self._opmanager = OperationManager()
-        opnames = self._opmanager.getOperationNames()
-        self.set_usage("usage: %prog [options] data output")
-        self.add_options((
-          Option( "-c", "--config", dest="config", type="string",
+    _opmanager = OperationManager()
+
+    output_format_choices = (FIDL_FORMAT, VOXBO_FORMAT, ANALYZE_FORMAT)
+    output_datatype_choices= (MAGNITUDE_TYPE, COMPLEX_TYPE)
+    options = (
+
+          Option("-c", "--config", dest="config", type="string",
             default="recon.cfg", action="store",
             help="Name of the config file describing operations and operation"\
             " parameters."),
-          Option( "-r", "--vol-range", dest="vol_range", type="string", default=":",
+
+          Option("-r", "--vol-range", dest="vol_range", type="string", default=":",
             action="store",
             help="Which image volumes to reconstruct.  Format is start:end, where "\
             "either start or end may be omitted, indicating to start with the "\
@@ -42,22 +29,34 @@ class Recon (OptionParser):
             "volume is 0.  The default value is a single colon with no start "\
             "or end specified, meaning process all volumes.  (Note, this option "\
             "refers specifically to image volumes, not to reference scans.)"),
-          Option( "-f", "--file-format", dest="file_format", action="store",
+
+          Option("-f", "--file-format", dest="file_format", action="store",
             type="choice", default=ANALYZE_FORMAT, choices=output_format_choices,
             help="""{%s}
             fidl: save floating point file with interfile and 4D analyze headers.
             analyze: Save individual image for each frame in analyze format.
-            voxbo: Save in tes format."""%("|".join(output_format_choices)) ),
-          Option( "-t", "--tr", dest="TR", type="float", action="store",
-            help="Use the TR given here rather than the one in the procpar." ),
-          Option( "-y", "--output-data-type", dest="output_data_type",
+            voxbo: Save in tes format."""%("|".join(output_format_choices))),
+
+          Option("-t", "--tr", dest="TR", type="float", action="store",
+            help="Use the TR given here rather than the one in the procpar."),
+
+          Option("-y", "--output-data-type", dest="output_data_type",
             type="choice", default=MAGNITUDE_TYPE, action="store",
             choices=output_datatype_choices,
             help="""{%s}
             Specifies whether output images should contain only magnitude or
             both the real and imaginary components (only valid for analyze
-            format)."""%("|".join(output_datatype_choices)) )
-        ))
+            format)."""%("|".join(output_datatype_choices))),
+
+          Option("-l", "--log-file", default="recon.log",
+            help="where to record reconstruction details ('recon.log' by default)")
+        )
+
+    #-------------------------------------------------------------------------
+    def __init__(self, *args, **kwargs):
+        OptionParser.__init__(self, *args, **kwargs)
+        self.set_usage("usage: %prog [options] data output")
+        self.add_options(self.options)
 
     #-------------------------------------------------------------------------
     def configureOperations(self, configfile):
@@ -108,10 +107,12 @@ class Recon (OptionParser):
 
     #-------------------------------------------------------------------------
     def run(self):
-        "Run the epi_recon tool."
+        "Run the recon tool."
 
         # Get the filename names and options from the command line.
         options = self.getOptions()
+
+        runlogger = RunLogger(file("recon.log",'w')) # file(options.log_file, 'w')
 
         # Load k-space image from the fid file.
         image = FidImage(options.datadir, options.TR)
@@ -119,13 +120,11 @@ class Recon (OptionParser):
         # Log some parameter info to the console.
         image.logParams()
 
-        # Now apply the various data manipulation and artifact correction
-        # operations to the time-domain (k-space) data which is stored in the
-        # image attributes data and nav_data as well as the ancillary data arrays
-        # ref_data and ref_nav_data. The operations are applied by looping over
-        # the list of operations specified in the config file. Each operation
-        # acts independently upon the data.
-        for operation, args in options.operations: operation(**args).run(image)
+        # Apply operations to the data
+        for operation_class, args in options.operations:
+            operation = operation_class(**args)
+            operation.run(image)
+            runlogger.logop(operation)
 
         # Save data to disk.
         image.save(options.outfile, options.file_format, options.output_data_type)
