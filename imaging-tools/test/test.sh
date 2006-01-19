348fc321f35f08bacb571fed27bc1f5bd2b88ebb
Brian Hawthorne <brian.lee.hawthorne@gmail.com>
2006-01-19 05:31:27 +0000

    organize test materials
    git-svn-id: file:///home/mb312/repositories/public-repo/bic/trunk@698 c5881a9f-4ef2-0310-b211-a1cdbd18b07b

--- imaging-tools/test/test.sh
+++ imaging-tools/test/test.sh
@@ -4,7 +4,8 @@ recon_cmd=./scripts/recon
 testdata_dir=../testdata
 output_dir=./Images
 #dataset=Vari_ss_epi
-dataset=Ravi_ns22
+dataset=asems
+#dataset=Ravi_ns22
 
 mkdir -p $output_dir
 python $recon_cmd --config=recon.ops $testdata_dir/$dataset.fid ./$output_dir/${dataset}_recon 
