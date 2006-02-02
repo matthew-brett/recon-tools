770de838e4c78e702b270d59ac00715c19efcc13
Mike Trumpis <mtrumpis@gmail.com>
2006-02-02 21:49:49 +0000

    more polite references to image attributes
    git-svn-id: file:///home/mb312/repositories/public-repo/bic/trunk@729 c5881a9f-4ef2-0310-b211-a1cdbd18b07b

--- imaging-tools/root/imaging/operations/Rot90.py
+++ imaging-tools/root/imaging/operations/Rot90.py
@@ -1,27 +1,30 @@
+"This module defines a rotation on all slices, putting them into standard radiological format"
+
 from pylab import zeros, reshape, Complex32, arange, take, ravel, transpose
 from imaging.operations import Operation, Parameter
+from imaging.imageio import get_dims
 
 #functioning
-#TODO: change n_fe, n_pe variables ( I think n_pe = y, n_fe = x, ask Brian )
-#      perhaps expand to an arbitrary n*pi/2 rotation?
+#TODO: perhaps expand to an arbitrary n*pi/2 rotation?
 
-def rotateIm2d(im2d):
+def rotateIm2d(im2d, rowSize, colSize):
     """
     This method simply takes a 2D matrix and rotates it +90 degrees (equivalent
     to an upside-down transpose).
     @param im2d: a 2D MxN matrix
-    @returns: a 2D NxM matrix
+    @param rowSize: := M (number of rows)
+    @param colSize: := N (number of columns)
+    @returns: a 2D NxM matrix (for now)
     """
 
-    (xsize, ysize) = im2d.shape
-    twist = zeros((ysize,xsize), im2d.typecode())
-    rev = -1 - arange(ysize)
-    for row in range(xsize):
+    twist = zeros((colSize,rowSize), im2d.typecode())
+    rev = -1 - arange(colSize)
+    for row in range(rowSize):
         #this is good for -90 degrees: 
-        #twist[:,-(row+1)] = reshape(take(im2d[row], rev), (ysize,1))[:,0]
+        #twist[:,-(row+1)] = reshape(take(im2d[row], rev), (colSize,1))[:,0]
 
         #this is good for +90 degrees:
-        twist[:,row] = reshape(take(im2d[row], rev), (ysize,1))[:,0]
+        twist[:,row] = reshape(take(im2d[row], rev), (colSize,1))[:,0]
 
         #this may be good for 180 degrees (untested, assumes logic to shape twist correctly):
         #twist[-(row+1),:] = take(im2d[row], rev)[0,:]
@@ -35,11 +38,17 @@ class Rot90 (Operation):
     """
 
     def run(self, image):
-        (nvol, nslices, n_pe, n_fe) = image.data.shape
-                
+        #data dimensions haven't been set yet?
+        image.setData(image.data)
+        (nvols, nslices, nrows, ncols) = (image.tdim, image.zdim, image.ydim, image.xdim)        
         for vol in image.data:
             for slice in vol:
-                slice.flat[:] = rotateIm2d(slice).flat
-        image.data = reshape(image.data, (nvol, nslices, n_fe, n_pe))
+                #nrows,ncols may be backwards, I'm still confused why nrows = y, ncols = x
+                slice.flat[:] = rotateIm2d(slice, nrows, ncols).flat
+
+        #need to swap the way Python indexes the array  
+        #this might be changed when there is something callable for a BaseImage:
+        image.data = reshape(image.data, (nvols, nslices, ncols, nrows))
+        image.setData(image.data)
                 
     #done
