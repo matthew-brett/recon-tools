636b74973bf9b34002f1124f63d8368db8f1e7f8
Brian Hawthorne <brian.lee.hawthorne@gmail.com>
2006-02-03 05:25:28 +0000

    convert to use OptionParser for command-line option parsing
    git-svn-id: file:///home/mb312/repositories/public-repo/bic/trunk@736 c5881a9f-4ef2-0310-b211-a1cdbd18b07b

--- imaging-tools/root/imaging/tools/Fdf2Img.py
+++ imaging-tools/root/imaging/tools/Fdf2Img.py
@@ -1,18 +1,30 @@
 import os
 import sys
-from imaging.analyze import AnalyzeWriter
-from imaging.imageio import write_analyze
+from optparse import OptionParser
+
 from imaging.varian.FDFImage import FDFImage
 from imaging.operations.ReorderSlices import ReorderSlices
 from imaging.operations.FlipSlices import FlipSlices
-from imaging.operations.ViewImage import ViewImage
 
 ##############################################################################
 class Fdf2Img (object):
 
+    # command line option parser
+    parser = OptionParser(usage=\
+      "  usage: %prog [options] fdf_directory\n"\
+      "  Convert the the Varian FDF image in the given fdf_directory\n"\
+      "  (.dat) into an Analyze image.  The Analyze dataset will be\n"\
+      "  placed in a new directory with the same name but a .img extension\n"\
+      "  instead of .dat.")
+
     #-------------------------------------------------------------------------
     def run(self):
-        datadir = sys.argv[1]
+        _, args = self.parser.parse_args()
+
+        if not args:
+            self.parser.print_help()
+            sys.exit(0)
+        else: datadir = args[0]
         outputdir = datadir.rsplit(".",1)[0]+".img"
 
         # make sure the output directory is in place
@@ -21,5 +33,4 @@ class Fdf2Img (object):
         fdfimage = FDFImage(datadir)
         ReorderSlices().run(fdfimage)
         FlipSlices(flipud=False, fliplr=True).run(fdfimage)
-        #ViewImage().run(fdfimage)
         fdfimage.save(outputdir)
