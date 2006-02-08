ee5a00ccdeee8e257e6663720311850e1457439f
Brian Hawthorne <brian.lee.hawthorne@gmail.com>
2006-02-08 01:30:49 +0000

    - add new viewimage tool to the distribution - cosmetic cleanup
    git-svn-id: file:///home/mb312/repositories/public-repo/bic/trunk@754 c5881a9f-4ef2-0310-b211-a1cdbd18b07b

--- imaging-tools/setup.py
+++ imaging-tools/setup.py
@@ -2,13 +2,28 @@
 from distutils.core import setup
 
 setup(
+
   name='python-imaging-tools',
   version='0.2',
-  description='Tools for manipulating MRI data, specializing in k-space reconstruction.',
+  description=\
+   'Tools for manipulating MRI data, specializing in k-space reconstruction.',
   author='Brian Hawthorne',
   author_email='brian.lee.hawthorne@gmail.com',
   url='http://cirl.berkeley.edu/view/BIC/ImagingTools',
+
   package_dir = {'':'root'},
-  packages=['', 'imaging', 'imaging.operations', 'imaging.tools', 'imaging.varian'],
-  scripts=['scripts/dumpheader', 'scripts/getparam', 'scripts/fdf2img', 'scripts/recon']
+
+  packages=[
+    '',
+    'imaging',
+    'imaging.operations',
+    'imaging.tools',
+    'imaging.varian'],
+
+  scripts=[
+    'scripts/dumpheader',
+    'scripts/getparam',
+    'scripts/fdf2img',
+    'scripts/viewimage',
+    'scripts/recon']
 )
