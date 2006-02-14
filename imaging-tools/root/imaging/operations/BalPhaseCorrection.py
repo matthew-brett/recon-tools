b843cb64079f852a31fac6996afd9a220da95209
Mike Trumpis <mtrumpis@gmail.com>
2006-02-14 01:20:54 +0000

    Performs balanced phase correction
    git-svn-id: file:///home/mb312/repositories/public-repo/bic/trunk@766 c5881a9f-4ef2-0310-b211-a1cdbd18b07b

---
+++ imaging-tools/root/imaging/operations/BalPhaseCorrection.py
@@ -0,0 +1,23 @@
+"Applies a Balanced Phase Correction on data sets with two reference volumes"
+
+from FFT import inverse_fft
+from pylab import angle, conjugate, sin, cos, Complex32, fft
+from imaging.operations import Operation
+
+class BalPhaseCorrection (Operation):
+
+    def run(self, image):
+        if len(image.ref_vols) < 2:
+            self.log("Not enough reference volumes, quitting.")
+            return
+
+        #find point-by-point the image-space angle between 2nd and 1st reference data, divide by two
+        #fft & inverse_fft go row-by-row on arrays, so a one-liner:
+        ref_phs = angle(conjugate(inverse_fft(image.ref_data[0]))*inverse_fft(image.ref_data[1]))/2
+
+        #define the correction to subtract this angle
+        correction = cos(ref_phs) - 1.0j*sin(ref_phs)
+
+        #apply the correction in image space, take back to k space (correction volume
+        #gets repeatedly multiplied to any volume in image.data)
+        image.data = fft(inverse_fft(image.data)*correction).astype(Complex32)
