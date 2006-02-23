7ef261b8b59916b81168368defb1eb6019923f82
Brian Hawthorne <brian.lee.hawthorne@gmail.com>
2006-02-23 00:29:35 +0000

    move fermi_filter function into imaging.util module
    git-svn-id: file:///home/mb312/repositories/public-repo/bic/trunk@776 c5881a9f-4ef2-0310-b211-a1cdbd18b07b

--- imaging-tools/root/imaging/operations/FermiFilter.py
+++ imaging-tools/root/imaging/operations/FermiFilter.py
@@ -1,21 +1,6 @@
 from pylab import frange, meshgrid, exp, sqrt, Float32
 from imaging.operations import Operation, Parameter
-
-
-##############################################################################
-def fermi_filter(rows, cols, cutoff, trans_width):
-    """
-    @return: a Fermi filter kernel.
-    @param cutoff: distance from the center at which the filter drops to 0.5.
-      Units for cutoff are percentage of radius.
-    @param trans_width: width of the transition.  Smaller values will result
-      in a sharper dropoff.
-    """
-    row_end = (rows-1)/2.0; col_end = (cols-1)/2.0
-    row_vals = frange(-row_end, row_end)**2
-    col_vals = frange(-col_end, col_end)**2
-    X, Y = meshgrid(row_vals, col_vals)
-    return 1/(1 + exp((sqrt(X + Y) - cutoff*cols/2.0)/trans_width))
+from imaging.util import fermi_filter
 
 
 ##############################################################################
