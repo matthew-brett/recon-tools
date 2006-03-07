890fa91214ab5b0c087f737ce1d944faf7b1254c
Brian Hawthorne <brian.lee.hawthorne@gmail.com>
2006-03-07 23:26:57 +0000

    small bugfix plus comment
    git-svn-id: file:///home/mb312/repositories/public-repo/bic/trunk@808 c5881a9f-4ef2-0310-b211-a1cdbd18b07b

--- imaging-tools/test/test-config.sh
+++ imaging-tools/test/test-config.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# Shared configuration for test scripts to use.
 export output_dir=./test-output
 export scripts_dir=../scripts
 export testdata_dir=../../testdata
--- imaging-tools/test/test-recon.sh
+++ imaging-tools/test/test-recon.sh
@@ -2,7 +2,8 @@
 . ./test-config.sh
 #dataset=Vari_ss_epi
 dataset=asems
+#dataset=epi_1sh_lin_64x64
 #dataset=Ravi_ns22
 
 mkdir -p $output_dir
-python $scripts_dir/recon --config=recon.ops $testdata_dir/$dataset.fid ./$output_dir/${dataset}_recon 
+python $scripts_dir/recon --config=recon.ops $testdata_dir/$dataset.fid $output_dir/${dataset}_recon 
