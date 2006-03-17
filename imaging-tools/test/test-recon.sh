30d20e3e81c473e41d47b27a876674ca0dd1338a
Mike Trumpis <mtrumpis@gmail.com>
2006-03-17 01:58:02 +0000

    stock oplists
    git-svn-id: file:///home/mb312/repositories/public-repo/bic/trunk@845 c5881a9f-4ef2-0310-b211-a1cdbd18b07b

--- imaging-tools/test/test-recon.sh
+++ imaging-tools/test/test-recon.sh
@@ -5,14 +5,16 @@ dataset=asems
 #dataset=Ravi_ns22
 #dataset=Varian_ns22
 #dataset=epi_1sh_lin_64x64
-#dataset=Bal_phs_corr2/epidw_PN
+#dataset=Bal_phs_corr/epidw_PN
+#dataset=epidw_one_ref
 #dataset=mp_flash3d
+#dataset=gems
 #dataset=gems_anat
 #dataset=SSFP/mp_flash3d_1volume_nsccn
 #dataset=SSFP/mp_flash3d_10volumes_nsccn
 #dataset=SSFP/mp_flash3d_64x64x32_raw
 
 mkdir -p $output_dir
-python $scripts_dir/recon recon.ops \
+python $scripts_dir/recon \
        $testdata_dir/$dataset.fid \
        $output_dir/$dataset.recon
