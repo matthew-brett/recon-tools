e14518e456fc6797f65ff5f99ae8e06c05116a6a
Brian Hawthorne <brian.lee.hawthorne@gmail.com>
2006-03-17 10:52:08 +0000

    bugfix getConfiFileName function
    git-svn-id: file:///home/mb312/repositories/public-repo/bic/trunk@849 c5881a9f-4ef2-0310-b211-a1cdbd18b07b

--- imaging-tools/root/imaging/conf/__init__.py
+++ imaging-tools/root/imaging/conf/__init__.py
@@ -3,14 +3,14 @@ The imaging.conf package exists to contain configuration data files.  It does
 not contain any python modules.  The package defines a single function which
 can retrieve these config files by name.
 """
-from os.path import dirname, join
+from os.path import dirname, join, exists
 
 #-----------------------------------------------------------------------------
 def getConfigFileName(name):
     "Resolves the given config name into a full file path to that config file"
     confdir = dirname(__file__)
     filename = join(confdir, name)
-    if not os.path.exists(filename):
+    if not exists(filename):
         raise ValueError("File %s not found in config directory %s"\
                          %(name,confdir))
-    else return file(filename)
+    return filename
