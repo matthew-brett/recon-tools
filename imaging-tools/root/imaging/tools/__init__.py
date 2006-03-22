cb85af854a3689fc8e15b510dfb5024bb8d208f4
Brian Hawthorne <brian.lee.hawthorne@gmail.com>
2006-03-22 02:32:54 +0000

    add map from tool name to class name
    git-svn-id: file:///home/mb312/repositories/public-repo/bic/trunk@878 c5881a9f-4ef2-0310-b211-a1cdbd18b07b

--- imaging-tools/root/imaging/tools/__init__.py
+++ imaging-tools/root/imaging/tools/__init__.py
@@ -1,21 +1,29 @@
 from ConfigParser import SafeConfigParser
+from odict import odict
+
+tool_map = odict((
+  ("dumpheader", "imaging.tools.DumpHeader"),
+  ("fdf2img", "imaging.tools.Fdf2Img"),
+  ("getparam", "imaging.tools.GetParam"),
+  ("imaging-doc", "imaging.tools.ImagingDoc"),
+  ("recon", "imaging.tools.Recon"),
+  ("viewimage", "imaging.tools.ViewImage")))
+
 
 ##############################################################################
 class OrderedConfigParser (SafeConfigParser):
     """
     Config parser which keeps track of the order in which sections appear.
     
-    This class extends ConfigParser just slightly, giving it an ordered dictionary 
-    to store sections, so that operations listed in the config file are performed 
-    in the correct order. Otherwise, the functionality is that of ConfigParser.
-    
-    @cvar _sections: an odict with entries for given sections (opnames) and their related items (params)
+    This class extends ConfigParser just slightly, giving it an ordered
+    dictionary to store sections, so that operations listed in the config file
+    are performed in the correct order.  Otherwise, the functionality is that
+    of ConfigParser.
     """
 
     #-------------------------------------------------------------------------
     def __init__(self, defaults=None):
         SafeConfigParser.__init__(self, defaults=defaults)
-        import odict
-        self._sections = odict.odict()
+        self._sections = odict()
 
 
