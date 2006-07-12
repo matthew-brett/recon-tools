1dfb75d47428b15667b5a083ef73205b6de1177f
Mike Trumpis <mtrumpis@gmail.com>
2006-07-12 23:12:14 +0000

    rolled back unintended changes
    git-svn-id: file:///home/mb312/repositories/public-repo/bic/trunk@1037 c5881a9f-4ef2-0310-b211-a1cdbd18b07b

--- imaging-tools/root/imaging/punwrap/__init__.py
+++ imaging-tools/root/imaging/punwrap/__init__.py
@@ -3,11 +3,7 @@ try:
 except ImportError:
     raise ImportError("Please compile the punwrap extension to use this module")
 
-from _punwrap_old import lpUnwrap as lpUnwrap2
-
 import Numeric
-
-#masking in _punwrap.so doesn't seem to work well
 
 def unwrap2D(matrix, mask=None):
     """The method for this module unwraps a 2D grid of wrapped phases
@@ -23,39 +19,11 @@ def unwrap2D(matrix, mask=None):
     if len(dims)>2: raise ValueError("matrix has too many dimensions to unwrap")
     if mask is None: mask = Numeric.ones(dims)
     else:
-        #print "warning: masking feature has unpredictable results"
         if dims != mask.shape:
             raise ValueError("mask dimensions do not match matrix dimensions!")
     
     in_phase = len(dims) < 2 and reshape(matrix,(1,dims[0])) or matrix
-    #in_phase = ( (in_phase/2/Numeric.pi + 1)%1 ).astype(Numeric.Float32)
-    #ret = (lpUnwrap(in_phase, mask.astype(Numeric.Int8))*2*Numeric.pi).astype(dtype)
     in_phase = (in_phase*mask).astype(dtype)
     ret = (lpUnwrap(in_phase.astype(Numeric.Float32))).astype(dtype)
     return ret
 
-def unwrap2D2(matrix, mask=None):
-    """The method for this module unwraps a 2D grid of wrapped phases
-    using the lp-norm method.
-    @param matrix, if ndim > 2, explode; if ndim < 2, a 1xN matrix
-    is used. Numerical range should be [-pi,pi]
-    @return: the unwrapped phases
-    """
-
-    dtype = matrix.typecode()
-    dims = matrix.shape
-
-    if len(dims)>2: raise ValueError("matrix has too many dimensions to unwrap")
-    if mask is None: mask = Numeric.ones(dims)
-    else:
-        #print "warning: masking feature has unpredictable results"
-        if dims != mask.shape:
-            raise ValueError("mask dimensions do not match matrix dimensions!")
-    
-    in_phase = len(dims) < 2 and reshape(matrix,(1,dims[0])) or matrix
-    in_phase = ( (in_phase/2/Numeric.pi + 1)%1 ).astype(Numeric.Float32)
-    #in_phase = (in_phase*mask).astype(Numeric.Float32)
-    ret = (lpUnwrap2(in_phase, mask.astype(Numeric.UInt8))*2*Numeric.pi).astype(dtype)
-
-    #ret = (lpUnwrap(in_phase.astype(Numeric.Float32))).astype(dtype)
-    return ret
